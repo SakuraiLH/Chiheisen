0ab54f955efe26bda4adfaeab7b2b65de0d05de4
SakuraiLH <hikarilh@foxmail.com>
2022-03-05 16:37:05 +0800

    Chiheisen: Import our information banner
    * Which should be imported a long time ago
    
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>

--- include/SoftwareString.h
+++ include/SoftwareString.h
@@ -1,3 +1,25 @@
+// Copyright <C> 2020-2022 SakuraiLH, All rights reserved.
+/*   ____ _   _ ___ _   _ _____ ___ ____  _____ _   _ 
+ *  / ___| | | |_ _| | | | ____|_ _/ ___|| ____| \ | |
+ * | |   | |_| || || |_| |  _|  | |\___ \|  _| |  \| |
+ * | |___|  _  || ||  _  | |___ | | ___) | |___| |\  |
+ *  \____|_| |_|___|_| |_|_____|___|____/|_____|_| \_|
+ *
+ *  Published using AGPLv3 license.
+ *  If your software can interact with users remotely through a computer
+ * network, you should also make sure that it provides a way for users to
+ * get its source.  For example, if your program is a web application, its
+ * interface could display a "Source" link that leads users to an archive
+ * of the code.  There are many ways you could offer source, and different
+ * solutions will be better for different programs; see section 13 for the
+ * specific requirements.
+ *
+ * You should also get your employer (if you work as a programmer) or school,
+ * if any, to sign a "copyright disclaimer" for the program, if necessary.
+ * For more information on this, and how to apply and follow the GNU AGPL, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ */
 #pragma once
 #include <iostream>
 
